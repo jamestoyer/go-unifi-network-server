d2668e13f2b42eba96b3f94c901f20bd4791fa5f
James Toyer <jamestoyer@users.noreply.github.com>
2024-10-05 18:44:22 +0100

    Add an integration tests framework (#23)
    * Create an integration test suite
    
    * Clean up the test suite code

---
+++ networkserver/client_test.go
@@ -0,0 +1,62 @@
+// Copyright 2024 James Toyer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package networkserver
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"testing"
+
+	"github.com/jamestoyer/go-unifi-network-server/networkserver/internal/testhelpers"
+	"github.com/stretchr/testify/suite"
+)
+
+type ClientIntegrationTestSuite struct {
+	suite.Suite
+
+	client         *Client
+	unifiContainer *testhelpers.UnifiNetworkServerContainer
+}
+
+func (suite *ClientIntegrationTestSuite) SetupSuite() {
+	ctx := context.Background()
+	container, err := testhelpers.NewUnifiNetworkServerContainer(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	suite.unifiContainer = container
+
+	client, err := NewClient(ctx, suite.unifiContainer.Endpoint, "admin", "admin",
+		WithTLSConfig(&tls.Config{InsecureSkipVerify: true}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	suite.client = client
+}
+
+func (suite *ClientIntegrationTestSuite) TearDownSuite() {
+	ctx := context.Background()
+	if err := suite.unifiContainer.Terminate(ctx); err != nil {
+		log.Fatalf("Error terminating Unifi container: %s", err)
+	}
+}
+
+func TestClientIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientIntegrationTestSuite))
+}
---
+++ networkserver/internal/testhelpers/container.go
@@ -0,0 +1,97 @@
+// Copyright 2024 James Toyer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testhelpers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+type UnifiNetworkServerContainer struct {
+	testcontainers.Container
+	Endpoint string
+}
+
+func NewUnifiNetworkServerContainer(ctx context.Context) (*UnifiNetworkServerContainer, error) {
+	// When set to "true" debug logs from the server will be output
+	unifiStdOut := ""
+	var logConsumers []testcontainers.LogConsumer
+	if stdOut := os.Getenv("UNIFI_STDOUT"); stdOut != "" {
+		unifiStdOut = stdOut
+	}
+
+	logConsumers = append(logConsumers, &testcontainers.StdoutLogConsumer{})
+
+	// TODO: (jtoyer) This should be loaded from the rendered details version, once that is available
+	unifiVersion := "v8.4.62"
+	if version := os.Getenv("UNIFI_VERSION"); version != "" {
+		unifiVersion = version
+	}
+
+	demoModePath, err := filepath.Abs("../dev/unifi/demo-mode")
+	if err != nil {
+		return nil, fmt.Errorf("could not get absolute path for demo-mode: %w", err)
+	}
+
+	r, err := os.Open(demoModePath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open Unifi Network Server configuration file: %w", err)
+	}
+
+	req := testcontainers.ContainerRequest{
+		Name:  fmt.Sprintf("unifi-%v", time.Now().Unix()),
+		Image: "jacobalberty/unifi:" + unifiVersion,
+		Env: map[string]string{
+			"UNIFI_STDOUT": unifiStdOut,
+		},
+		ExposedPorts: []string{"8443/tcp"},
+		Files: []testcontainers.ContainerFile{
+			{
+				Reader:            r,
+				HostFilePath:      demoModePath,
+				ContainerFilePath: "/usr/local/unifi/init.d/demo-mode",
+				FileMode:          0o700,
+			},
+		},
+		LogConsumerCfg: &testcontainers.LogConsumerConfig{
+			Consumers: logConsumers,
+		},
+		WaitingFor: wait.ForHealthCheck(),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to start Unifi Network Server container: %w", err)
+	}
+
+	endpoint, err := container.PortEndpoint(ctx, "8443/tcp", "https")
+	if err != nil {
+		return nil, fmt.Errorf("unable to get Unifi Network Server endpoint: %w", err)
+	}
+
+	return &UnifiNetworkServerContainer{
+		Container: container,
+		Endpoint:  endpoint,
+	}, nil
+}
